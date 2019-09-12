70161ad5b8d96fde6db46f804181e005a3048b99
Tami Reichold <reichold.tamara@gmail.com>
2019-09-12 14:42:37 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -3,6 +3,8 @@ require 'pry'
 def consolidate_cart(cart)
   final_hash = {}
   cart.each do |element_hash|
+    element_name = element_hash.keys(0)
+    element_stats = element_hash.values(0)
     binding.pry
   end
 end
