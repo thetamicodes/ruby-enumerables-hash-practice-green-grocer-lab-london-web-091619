d383979013fc99f8bd65912060bdd0b4c49c8a12
Tami Reichold <reichold.tamara@gmail.com>
2019-09-12 15:09:58 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -4,15 +4,14 @@ def consolidate_cart(cart)
   final_hash = {}
   cart.each do |element_hash|
     element_name = element_hash.keys[0]
-    element_stats = element_hash.values[0]
-    
+
     if final_hash.has_key?(element_name)
       final_hash[element_name][:count] += 1 
     else
       final_hash[element_name] = {
         count: 1,
         price: element_hash[element_name][:price],
-        clearance: element_stats[:clearance]
+        clearance: element_hash[element_name][:clearance]
       }
     end
   end
