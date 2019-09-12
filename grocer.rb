22882fdd06e79e0931c5738fcb0be002150f1fa8
Tami Reichold <reichold.tamara@gmail.com>
2019-09-12 14:51:05 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -3,9 +3,14 @@ require 'pry'
 def consolidate_cart(cart)
   final_hash = {}
   cart.each do |element_hash|
-    element_name = element_hash.keys(0)
-    element_stats = element_hash.values(0)
-    binding.pry
+    element_name = element_hash.keys[0]
+    element_stats = element_hash.values[0]
+    
+    if final_hash.has_key?(element_name)
+      element_stats[:count] += 1 
+    else
+    binding.pry  
+    end
   end
 end
 
