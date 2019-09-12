9250a8cab626a7e4a134937f1e57685f6c565e6b
Tami Reichold <reichold.tamara@gmail.com>
2019-09-12 14:53:02 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -9,6 +9,8 @@ def consolidate_cart(cart)
     if final_hash.has_key?(element_name)
       element_stats[:count] += 1 
     else
+      final_hash[element_name] = element_stats
+      final_hash[:count] = 1
     binding.pry  
     end
   end
