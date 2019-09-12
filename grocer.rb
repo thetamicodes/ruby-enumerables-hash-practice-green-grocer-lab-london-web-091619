94f860e30d30335096399823e8e62e178de3fda3
Tami Reichold <reichold.tamara@gmail.com>
2019-09-12 16:42:44 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -41,8 +41,12 @@ def apply_clearance(cart)
   cart
 end
 
-def checkout(cart, coupons)
-  # code here
+def checkout(array, coupons)
+  hash_cart = consolidate_cart(array)
+  applied_coupons = apply_coupons(hash_cart, coupons) 
+  applied_discount = apply_clearance(applied_coupons)
+  total = applied_discount.reduce(0) { |acc, (key, value)| acc += value[:price] * value[:count]}
+  total > 100 ? total * 0.9 : total 
 end
 
 
